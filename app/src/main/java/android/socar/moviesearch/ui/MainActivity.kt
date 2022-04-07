ced1dc9554f89360c6d468aa2adacd5e466edd89
JongDo-android <whdgus090@gmail.com>
2022-04-07 20:08:11 +0900

    feat: MainActivity MovieAdapter 연결

--- app/src/main/java/android/socar/moviesearch/ui/MainActivity.kt
+++ app/src/main/java/android/socar/moviesearch/ui/MainActivity.kt
@@ -5,21 +5,31 @@ import android.os.Bundle
 import android.socar.moviesearch.R
 import android.socar.moviesearch.databinding.ActivityMainBinding
 import android.socar.moviesearch.di.ViewModelFactory
+import android.socar.moviesearch.ui.adapter.MovieAdapter
+import android.socar.moviesearch.ui.adapter.MovieItemClickListener
 import android.socar.moviesearch.viewmodel.MainViewModel
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), MovieItemClickListener {
     private lateinit var binding: ActivityMainBinding
     private val mainViewModel: MainViewModel by lazy {
         ViewModelProvider(this, ViewModelFactory())[MainViewModel::class.java]
+    }
+    private val movieAdapter: MovieAdapter by lazy {
+        MovieAdapter(this)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.viewModel = mainViewModel
-
+        with(binding) {
+            viewModel = mainViewModel
+            rvSearchMovieResult.adapter = movieAdapter
+        }
+        mainViewModel.movieInformation.observe(this) {
+            movieAdapter.submitList(it)
+        }
         setClickListener()
     }
 
@@ -29,5 +39,9 @@ class MainActivity : AppCompatActivity() {
                 mainViewModel.fetchInformation(evMovieSearch.text.toString())
             }
         }
+    }
+
+    override fun onItemClick(url: String) {
+
     }
 }
