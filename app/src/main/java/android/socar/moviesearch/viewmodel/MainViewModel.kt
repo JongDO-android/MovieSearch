ef6839bc2de344af32409205c29f31e565621d16
JongDo-android <whdgus090@gmail.com>
2022-04-07 15:52:48 +0900

    feat: MainViewModel 추가

---
+++ app/src/main/java/android/socar/moviesearch/viewmodel/MainViewModel.kt
@@ -0,0 +1,10 @@
+package android.socar.moviesearch.viewmodel
+
+import android.socar.moviesearch.local.Repository
+import androidx.lifecycle.ViewModel
+
+class MainViewModel(repository: Repository): ViewModel() {
+
+    fun fetchInformation() {
+    }
+}
