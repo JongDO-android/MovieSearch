572fbc2f043bda4a799d844da62a9c9e4be1efa4
JongDo-android <whdgus090@gmail.com>
2022-04-07 17:12:11 +0900

    fix: NaverSearchService function suspend 키워드 추가

--- app/src/main/java/android/socar/moviesearch/remote/NaverSearchService.kt
+++ app/src/main/java/android/socar/moviesearch/remote/NaverSearchService.kt
@@ -1,15 +1,16 @@
 package android.socar.moviesearch.remote
 
+import android.socar.moviesearch.remote.dto.MovieResponse
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
 
 interface NaverSearchService {
-    @GET("/search/movie.json")
-    fun searchMovieInfo(
+    @GET("/v1/search/movie.json")
+    suspend fun searchMovieInfo(
         @Header("X-Naver-Client-Id") clientId: String,
         @Header("X-Naver-Client-Secret") clientSecret: String,
         @Query("query") search: String,
         @Query("display") display: Int = 50
-    )
+    ): MovieResponse
 }
