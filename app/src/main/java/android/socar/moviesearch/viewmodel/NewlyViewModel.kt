547c20e6b1383408de57ca74197437a653903b4c
JongDo-android <whdgus090@gmail.com>
2022-04-08 16:59:15 +0900

    feat: NewlyViewModel loadSearchInformation 함수 추가

--- app/src/main/java/android/socar/moviesearch/viewmodel/NewlyViewModel.kt
+++ app/src/main/java/android/socar/moviesearch/viewmodel/NewlyViewModel.kt
@@ -1,11 +1,17 @@
 package android.socar.moviesearch.viewmodel
 
+import android.content.Context
 import android.socar.moviesearch.local.LocalRepository
+import android.util.Log
 import androidx.lifecycle.ViewModel
 
 class NewlyViewModel(
     private val localRepository: LocalRepository
 ): ViewModel() {
 
-
+    fun loadSearchInformation(context: Context, addView: (search: String) -> Unit) {
+        localRepository.loadSearchInformation(context).forEach { searchKeyword ->
+            addView(searchKeyword)
+        }
+    }
 }
