4f941ff31d199ba31b166dc471f70cbbbfcfcbb5
JongDo-android <whdgus090@gmail.com>
2022-04-08 16:57:08 +0900

    feat: NewlyActivity 초기화 변수 추가

--- app/src/main/java/android/socar/moviesearch/ui/NewlyActivity.kt
+++ app/src/main/java/android/socar/moviesearch/ui/NewlyActivity.kt
@@ -3,14 +3,37 @@ package android.socar.moviesearch.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.socar.moviesearch.R
-import android.util.Log
+import android.socar.moviesearch.databinding.ActivityNewlyBinding
+import android.socar.moviesearch.di.ViewModelFactory
+import android.socar.moviesearch.viewmodel.NewlyViewModel
+import android.widget.Button
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 
 class NewlyActivity : AppCompatActivity() {
+    private val binding: ActivityNewlyBinding by lazy {
+        DataBindingUtil.setContentView(this, R.layout.activity_newly)
+    }
+    private val newlyViewModel: NewlyViewModel by lazy {
+        ViewModelProvider(this, ViewModelFactory())[NewlyViewModel::class.java]
+    }
+    private val addView = { search: String ->
+        val button = Button(this).apply {
+            text = search
+            setTextColor(ContextCompat.getColor(this@NewlyActivity, R.color.white))
+            background = ContextCompat.getDrawable(
+                this@NewlyActivity,
+                R.drawable.button_shape
+            )
+        }
+        binding.lvSearchLogList.addView(button)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_newly)
-
-        val sharedPref = getSharedPreferences(MainActivity.PREFERENCE_KEY, MODE_PRIVATE)
-        Log.i("NewlyActivity :", sharedPref.getString(MainActivity.PREFERENCE_KEY, "") ?: "")
+        binding
+        newlyViewModel.loadSearchInformation(this, addView)
     }
+
 }
