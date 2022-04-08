2cc0e9fc8d9f324927af593361807fb73a804453
JongDo-android <whdgus090@gmail.com>
2022-04-08 14:35:20 +0900

    feat: NewlyViewModel 추가

---
+++ app/src/main/java/android/socar/moviesearch/viewmodel/NewlyViewModel.kt
@@ -0,0 +1,11 @@
+package android.socar.moviesearch.viewmodel
+
+import android.socar.moviesearch.local.LocalRepository
+import androidx.lifecycle.ViewModel
+
+class NewlyViewModel(
+    private val localRepository: LocalRepository
+): ViewModel() {
+
+
+}
