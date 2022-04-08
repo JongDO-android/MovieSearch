ac0a7f761cd526173f3d81b8b3195c8c9bf6def0
JongDo-android <whdgus090@gmail.com>
2022-04-08 14:32:48 +0900

    fix: ViewModelFactory ViewModel 프로퍼티 수정

--- app/src/main/java/android/socar/moviesearch/di/ViewModelFactory.kt
+++ app/src/main/java/android/socar/moviesearch/di/ViewModelFactory.kt
@@ -2,16 +2,21 @@ package android.socar.moviesearch.di
 
 import android.socar.moviesearch.local.SearchRepository
 import android.socar.moviesearch.viewmodel.MainViewModel
+import android.socar.moviesearch.viewmodel.NewlyViewModel
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import java.lang.IllegalArgumentException
 
 class ViewModelFactory: ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return if(modelClass.isAssignableFrom(MainViewModel::class.java)) {
-            MainViewModel(SearchRepository()) as T
-        } else {
-            throw IllegalArgumentException()
+        return when {
+            modelClass.isAssignableFrom(MainViewModel::class.java) -> {
+                MainViewModel(SearchRepository(), SearchRepository()) as T
+            }
+            modelClass.isAssignableFrom(NewlyViewModel::class.java) -> {
+                NewlyViewModel(SearchRepository()) as T
+            }
+            else -> throw IllegalArgumentException()
         }
     }
 }
