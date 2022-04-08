9e7501bec8d0b2ad9978618fcee6c90be1626e7a
JongDo-android <whdgus090@gmail.com>
2022-04-08 14:33:36 +0900

    fix: SearchRepository LocalRepository 상속

--- app/src/main/java/android/socar/moviesearch/local/SearchRepository.kt
+++ app/src/main/java/android/socar/moviesearch/local/SearchRepository.kt
@@ -1,13 +1,18 @@
 package android.socar.moviesearch.local
 
+import android.content.Context
 import android.socar.moviesearch.common.NaverApi
 import android.socar.moviesearch.remote.RetrofitBuilder
 import android.socar.moviesearch.remote.dto.MovieInformation
 import android.socar.moviesearch.remote.Result
+import android.socar.moviesearch.ui.MainActivity
+import com.google.gson.Gson
 import retrofit2.HttpException
 
-class SearchRepository: Repository {
+class SearchRepository: RemoteRepository, LocalRepository {
     private val service = RetrofitBuilder.create()
+    private val _searchList = mutableListOf<String>()
+    override val searchList: List<String> get() = _searchList
 
     override suspend fun fetchMovieInfo(query: String): Result<List<MovieInformation>> {
         return try {
@@ -16,9 +21,30 @@ class SearchRepository: Repository {
                 NaverApi.CLIENT_SECRET,
                 query
             )
+            if(_searchList.size == 10) {
+                _searchList.removeAt(0)
+            }
+            _searchList.add(query)
             Result.Success(result.items)
         } catch (e: HttpException) {
             Result.Error(e)
         }
+    }
+
+    override fun saveSearchInformation(context: Context, list: List<String>) {
+        val sharedPref = context.getSharedPreferences(
+            MainActivity.PREFERENCE_KEY,
+            Context.MODE_PRIVATE
+        )
+
+        val searched = Gson().toJson(list)
+        with(sharedPref.edit()) {
+            putString(MainActivity.PREFERENCE_KEY, searched)
+            apply()
+        }
+    }
+
+    override fun loadSearchInformation(context: Context) {
+        
     }
 }
